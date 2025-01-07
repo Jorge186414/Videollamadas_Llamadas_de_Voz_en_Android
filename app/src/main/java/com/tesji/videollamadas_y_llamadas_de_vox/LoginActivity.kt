23917de4c61f9b5b060aa0b346dee3e4d1999030
Jorge Miranda <jorgaismiranda@gmail.com>
2025-01-06 23:20:40 -0600

    Add: LoginActivity

---
+++ app/src/main/java/com/tesji/videollamadas_y_llamadas_de_vox/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.tesji.videollamadas_y_llamadas_de_vox
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+    }
+}
