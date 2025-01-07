9a477828deb47b602bf84538f5df0ac736f54159
Jorge Miranda <jorgaismiranda@gmail.com>
2025-01-07 00:59:40 -0600

    Add: Configuracion de API de ZEGOCLOUD

--- app/src/main/java/com/tesji/videollamadas_y_llamadas_de_vox/LoginActivity.kt
+++ app/src/main/java/com/tesji/videollamadas_y_llamadas_de_vox/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.tesji.videollamadas_y_llamadas_de_vox
 
+import android.app.Application
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.tesji.videollamadas_y_llamadas_de_vox.databinding.ActivityLoginBinding
+import com.zegocloud.uikit.prebuilt.call.ZegoUIKitPrebuiltCallService
+import com.zegocloud.uikit.prebuilt.call.invite.ZegoUIKitPrebuiltCallInvitationConfig
 
 class LoginActivity : AppCompatActivity() {
 
@@ -17,6 +20,23 @@ class LoginActivity : AppCompatActivity() {
         binding.btnLogin.setOnClickListener{
             startActivity(Intent(applicationContext, MainActivity::class.java))
         }
+    }
 
+    // Configuracion de la API de ZEGOCLOUD
+    private fun setUpZegoUiKit(userId: String){
+        val application: Application = application
+        val appID : Long  = 111 // Tu appID de ZEGOCLOUD
+        val appSign : String = "Tu sign de ZEGOCLOUD";
+
+        val userName : String = userId;
+
+        val callInvitationConfig = ZegoUIKitPrebuiltCallInvitationConfig()
+
+        ZegoUIKitPrebuiltCallService.init(application, appID, appSign, userId, userName, callInvitationConfig)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ZegoUIKitPrebuiltCallService.unInit()
     }
 }
