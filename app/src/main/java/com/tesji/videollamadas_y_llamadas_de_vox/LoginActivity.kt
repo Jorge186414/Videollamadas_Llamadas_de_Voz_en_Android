c5f59be993d9786aaeb474396145d99acf8a982a
Jorge Miranda <jorgaismiranda@gmail.com>
2025-01-07 00:40:27 -0600

    Add: Vinculacion con la vista de MainActivity

--- app/src/main/java/com/tesji/videollamadas_y_llamadas_de_vox/LoginActivity.kt
+++ app/src/main/java/com/tesji/videollamadas_y_llamadas_de_vox/LoginActivity.kt
@@ -1,14 +1,22 @@
 package com.tesji.videollamadas_y_llamadas_de_vox
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.tesji.videollamadas_y_llamadas_de_vox.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+        }
 
     }
 }
